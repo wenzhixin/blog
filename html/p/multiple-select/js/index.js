46d9cc9607660082e6b965abba8676e8340cf875
zhixin <wenzhixin2010@gmail.com>
2013-05-19 00:44:13 +0800

    add project index

---
+++ html/p/multiple-select/js/index.js
@@ -0,0 +1,27 @@
+$(function() { 
+    $("#select1").multipleSelect();
+    
+    $("#select2").multipleSelect({
+    	selectAll: false
+    });
+    $('#setSelects').click(function() {
+    	$('#select2').multipleSelect('setSelects', [1, 3]);
+    });
+    $('#getSelects').click(function() {
+    	console.log($('#select2').multipleSelect('getSelects'));
+        alert('Selected value is: ' + $('#select2').multipleSelect('getSelects'));
+        alert('Selected text is: ' + $('#select2').multipleSelect('getSelects', 'text'));
+    });
+    $('#enable').click(function() {
+       $('#select2').multipleSelect('enable');
+    });
+    $('#disable').click(function() {
+       $('#select2').multipleSelect('disable');
+    });
+    
+    $("#select3").multipleSelect({
+    	isopen: true,
+    	multiple: true,
+    	multipleWidth: 55
+    });
+});
