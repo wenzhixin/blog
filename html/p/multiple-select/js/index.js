a16abd4ce518b49a00d98d8a491ac3039fb121cd
zhixin <wenzhixin2010@gmail.com>
2013-05-22 00:23:02 +0800

    update multiple-select project

--- html/p/multiple-select/js/index.js
+++ html/p/multiple-select/js/index.js
@@ -1,26 +1,175 @@
 $(function() { 
-    $("#select1").multipleSelect();
+    new Example($('#e1'), {
+        title: 'The Default',
+        items: ['January', 'February', 'March', 'April', 'May', 'June', 
+            'July', 'August', 'September', 'October', 'November', 'December'],
+        code: '\
+<head>\n\
+    <link href="multiple-select.css" rel="stylesheet"/>\n\
+</head>\n\
+<body>\n\
+    <select id="select">\n\
+        <option value="1">January</option>\n\
+        ...\n\
+        <option value="12">December</option>\n\
+    </select>\n\
+    <script src="jquery.multiple.select.js"></script>\n\
+    <script>\n\
+        $(function() { \n\
+            $("#select").multipleSelect(); \n\
+        });\n\
+    </script>\n\
+</body>'
+    }, function($select) {
+        $select.multipleSelect();
+    });
     
-    $("#select2").multipleSelect({
-    	selectAll: false
-    });
-    $('#setSelects').click(function() {
-    	$('#select2').multipleSelect('setSelects', [1, 3]);
-    });
-    $('#getSelects').click(function() {
-        alert('Selected value is: ' + $('#select2').multipleSelect('getSelects'));
-        alert('Selected text is: ' + $('#select2').multipleSelect('getSelects', 'text'));
-    });
-    $('#enable').click(function() {
-       $('#select2').multipleSelect('enable');
-    });
-    $('#disable').click(function() {
-       $('#select2').multipleSelect('disable');
+    
+    new Example($('#e2'), {
+        title: 'The Hide Select All',
+        items: ['First', 'Second', 'Third', 'Fourth'],
+        code: '\
+<head>\n\
+    <link href="multiple-select.css" rel="stylesheet"/>\n\
+</head>\n\
+<body>\n\
+    <select id="select">\n\
+        <option value="1">First</option>\n\
+        ...\n\
+        <option value="4">Fourth</option>\n\
+    </select>\n\
+    <script src="jquery.multiple.select.js"></script>\n\
+    <script>\n\
+        $(function() { \n\
+            $("#select").multipleSelect({\n\
+                selectAll: false\n\
+            })); \n\
+        });\n\
+    </script>\n\
+</body>'
+    }, function($select) {
+        $select.multipleSelect({
+            selectAll: false
+        });
     });
     
-    $("#select3").multipleSelect({
-    	isopen: true,
-    	multiple: true,
-    	multipleWidth: 55
+    
+    var days = [];
+    for (var i = 0; i < 31; i++) {
+        days.push(i + 1);
+    }
+    new Example($('#e3'), {
+        title: 'The Multiple Items',
+        items: days,
+        code: '\
+<head>\n\
+    <link href="multiple-select.css" rel="stylesheet"/>\n\
+</head>\n\
+<body>\n\
+    <select id="select">\n\
+        <option value="1">1</option>\n\
+        ...\n\
+        <option value="31">31</option>\n\
+    </select>\n\
+    <script src="jquery.multiple.select.js"></script>\n\
+    <script>\n\
+        $(function() { \n\
+            $("#select").multipleSelect({\n\
+                multiple: true,\n\
+                multipleWidth: 55\n\
+            })); \n\
+        });\n\
+    </script>\n\
+</body>'
+    }, function($select) {
+        $select.multipleSelect({
+            multiple: true,
+            multipleWidth: 55
+        });
+    });
+    
+    
+    new Example($('#e4'), {
+        title: 'The Placeholder',
+        items: ['January', 'February', 'March', 'April', 'May', 'June', 
+            'July', 'August', 'September', 'October', 'November', 'December'],
+        code: '\
+<head>\n\
+    <link href="multiple-select.css" rel="stylesheet"/>\n\
+</head>\n\
+<body>\n\
+    <select id="select">\n\
+        <option value="1">January</option>\n\
+        ...\n\
+        <option value="12">December</option>\n\
+    </select>\n\
+    <script src="jquery.multiple.select.js"></script>\n\
+    <script>\n\
+        $(function() { \n\
+            $("#select").multipleSelect({\n\
+            	placeholder: "Here is the placeholder"\n\
+            }); \n\
+        });\n\
+    </script>\n\
+</body>'
+    }, function($select) {
+        $select.multipleSelect({
+            placeholder: 'Here is the placeholder'
+        });
+    });
+    
+    
+    new Example($('#e5'), {
+        title: 'Programmatic Access',
+        items: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+        code: '\
+<head>\n\
+    <link href="multiple-select.css" rel="stylesheet"/>\n\
+</head>\n\
+<body>\n\
+    <button id="setSelects">Set selects</button>\n\
+    <button id="getSelects">Get selects</button>\n\
+    <button id="enable">Enable</button>\n\
+    <button id="disable">Disabled</button>\n\
+    <select id="select">\n\
+        <option value="1">Monday</option>\n\
+        ...\n\
+        <option value="7">Sunday</option>\n\
+    </select>\n\
+    <script src="jquery.multiple.select.js"></script>\n\
+    <script>\n\
+        $(function() { \n\
+            $select.multipleSelect();\n\
+            $("#setSelects").click(function() {\n\
+                $("#select2").multipleSelect("setSelects", [1, 3]);\n\
+            });\n\
+            $("#getSelects").click(function() {\n\
+                alert($("#select2").multipleSelect("getSelects"));\n\
+                alert("Selected text is: " + $("#select2").multipleSelect("getSelects", "text"));\n\
+            });\n\
+            $("#enable").click(function() {\n\
+               $("#select2").multipleSelect("enable");\n\
+            });\n\
+            $("#disable").click(function() {\n\
+               $("#select2").multipleSelect("disable");\n\
+            });\n\
+        });\n\
+    </script>\n\
+</body>'
+    }, function($select) {
+        $select.multipleSelect();
+        $('#setSelects').click(function() {
+            $select.multipleSelect('setSelects', [1, 3]);
+        });
+        $('#getSelects').click(function() {
+            alert($select.multipleSelect('getSelects'));
+            alert('Selected text is: ' + $select.multipleSelect('getSelects', 'text'));
+        });
+        $('#enable').click(function() {
+           $select.multipleSelect('enable');
+        });
+        $('#disable').click(function() {
+           $select.multipleSelect('disable');
+        });
     });
 });
