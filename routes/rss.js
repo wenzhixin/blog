0fef2b392ea065d6f4684563e75c08a44f1bd3f9
zhixin <wenzhixin2010@gmail.com>
2013-11-08 13:52:21 +0800

    RSS支持类型：全部、技术和生活随笔

--- routes/rss.js
+++ routes/rss.js
@@ -13,13 +13,14 @@ var fs = require('fs'),
 	POST_DIR = __dirname + '/../html/posts/';
 
 exports.list = function(req, res) {
-	var feed = new RSS({
-		title: '文翼的博客',
-		description: '简介：  坚持看书，每天进步一点点。 要常常提醒自己：无情的岁月，还有不够努力的自己！',
-		feed_url: config.site + '/rss.xml',
-		site_url: config.site,
-		author: 'wenzhixin'
-	});
+	var type = req.query.type || '',
+		feed = new RSS({
+			title: '文翼的博客',
+			description: '简介：  坚持看书，每天进步一点点。 要常常提醒自己：无情的岁月，还有不够努力的自己！',
+			feed_url: config.site + '/rss.xml',
+			site_url: config.site,
+			author: 'wenzhixin'
+		});
 	
 	rd.read(POST_DIR, function(err, files) {
 		if (err) {
@@ -34,15 +35,31 @@ exports.list = function(req, res) {
 		for (var i in files) {
 			var content = fs.readFileSync(files[i]).toString();
 				lines = content.split('\n'),
-				m = lines[2].match(/\d{2,4}-\d{1,2}-\d{1,2} \d{1,2}:\d{1,2}:\d{1,2}/);
+				category = lines[2].split(' | ')[0].split('：')[1],
+				m = lines[2].match(/\d{2,4}-\d{1,2}-\d{1,2} \d{1,2}:\d{1,2}:\d{1,2}/),
+				item = {
+					title: lines[0].substring(3),
+					description: marked(content),
+					url: feed.site_url + files[i].substring(POST_DIR.length - 11, files[i].length - 3),
+					author: 'wenzhixin',
+					date: m ? util.getGMTString(m[0]) : ''
+				};
 				
-			feed.item({
-				title: lines[0].substring(3),
-				description: marked(content),
-				url: feed.site_url + files[i].substring(POST_DIR.length - 11, files[i].length - 3),
-				author: 'wenzhixin',
-				date: m ? util.getGMTString(m[0]) : ''
-			});
+			switch (type) {
+			case 'tech':
+				if (util.getCategoryKey(category) !== 'life') {
+					feed.item(item);
+				}
+				break;
+			case 'life':
+				if (util.getCategoryKey(category) === 'life') {
+					feed.item(item);
+				}
+				break;
+			default:
+				feed.item(item);
+				break;
+			}
 		}
 		res.status(200);
 		res.end(feed.xml());
