635a84bbbc2874d9f18b4aab2483a3fb19f70260
zhixin <wenzhixin2010@gmail.com>
2018-12-31 17:08:17 +0800

    Update CNAME

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ SOURCE_BRANCH="master"
 TARGET_BRANCH="gh-pages"
 
 function doCompile {
-    hugo -b http://wenzhixin.net.cn/
+    hugo -b http://blog.wenzhixin.net.cn/
 }
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
