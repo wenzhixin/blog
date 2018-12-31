ebe97066e4a4834fe0252c9c36937b38a497bb02
zhixin <wenzhixin2010@gmail.com>
2018-12-31 17:03:10 +0800

    Fix output dir

--- deploy.sh
+++ deploy.sh
@@ -19,7 +19,7 @@ fi
 REPO=`git config remote.origin.url`
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 SHA=`git rev-parse --verify HEAD`
-OUT=_gh_pages
+OUT=public
 
 # Clone the existing gh-pages for this repo into dist/
 # Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
